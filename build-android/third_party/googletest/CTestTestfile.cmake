443b73c942e6bce3c8ac80d98e645623c30e3354
katolikov <55280831+katolikov@users.noreply.github.com>
2026-06-24 21:54:05 +0300

    chore: untrack build-android/ and generated assets; tighten .gitignore

--- build-android/third_party/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemkatolikov/DEV/LibTAS/third_party/googletest
-# Build directory: /Users/artemkatolikov/DEV/LibTAS/build-android/third_party/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build-android/third_party/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemkatolikov/DEV/LibTAS/third_party/googletest/googletest
-# Build directory: /Users/artemkatolikov/DEV/LibTAS/build-android/third_party/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
